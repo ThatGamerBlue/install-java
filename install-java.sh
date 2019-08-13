9dc23b49edbc44b0994c99a65ae6c4a64b023f13
ThatGamerBlue <thatgamerblue@gmail.com>
2019-08-13 10:32:25 +0100

    Update install-java.sh

--- install-java.sh
+++ install-java.sh
@@ -33,16 +33,7 @@ function usage() {
 }
 
 function confirm() {
-    # call with a prompt string or use a default
-    read -r -p "${1:-Are you sure?} [y/N] " response
-    case $response in
-    [yY][eE][sS] | [yY])
-        true
-        ;;
-    *)
-        false
-        ;;
-    esac
+    true
 }
 
 # Make sure the script is running as root.
